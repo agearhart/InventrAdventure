b7d916f8b6aad78a893c10caa0dd8d6019e19057
Andy Gearhart <andy.gear@gmail.com>
2023-01-03 11:09:13 -0800

    Lesson 2
    Use the HERO and breadboard to control an LED

---
+++ 02_Lights/lights/lights.ino
@@ -0,0 +1,19 @@
+// Code written by Inventr.io for the 30 Day Adventure Kit
+// Learn more at https://inventr.io/adventure
+
+
+int Light = 12; // The HERO Board Pin that the LED is wired to
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(Light, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(Light, HIGH);
+  delay(500);
+  digitalWrite(Light, LOW);
+  delay(500);
+}
